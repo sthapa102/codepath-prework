0319c0b2f06b26c4b8a9562a9a47578dcc091c3e
sthapa102 <82973044+sthapa102@users.noreply.github.com>
2023-07-16 16:07:56 -0400

    Submitted IOS CODEPATCH PREWORK files.

---
+++ PreWorks/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  PreWorks
+//
+//  Created by Sojal on 7/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var changeBackgroundColor: UIButton!
+    @IBAction func changeBackgroundColorTapped(_ sender: UIButton) {
+            let randomColor = changeColor()
+            view.backgroundColor = randomColor
+        }
+    
+    func changeColor() -> UIColor{
+
+           let red = CGFloat.random(in: 0...1)
+           let green = CGFloat.random(in: 0...1)
+           let blue = CGFloat.random(in: 0...1)
+
+           return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    
+       }
+    
+    
+
+}
+
